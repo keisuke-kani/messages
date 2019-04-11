43c658eb779ec7da07000e1cf340c064ae2992af
keisuke.kani <keisuke.kani@kiramex.com>
2019-04-11 08:57:40 +0000

    bootstrap design

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
-    @messages = Message.all
+#    @messages = Message.all.page(params[:page]).per(30)
+    @messages = Message.order(id: :desc).page(params[:page]).per(10)
   end
   
   def show
